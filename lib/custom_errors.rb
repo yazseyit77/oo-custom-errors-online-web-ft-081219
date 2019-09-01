d00215c93d47f26076daded64284233e36bf348d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 18:20:26 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,7 +9,10 @@ class Person
     self.partner = person
     if person.class != Person
       begin
-      raise PartnerError
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+    end
     else
       person.partner = self
     end
